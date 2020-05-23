3e435916c40ec3aa755599f8d491b6f2b26ebb9d
Harold Pedroso <haropedro@hotmail.com>
2020-05-22 18:51:46 -0700

    add association between user and articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,6 @@
 class Article < ActiveRecord::Base
-  validates :title, presence: true
-  validates :description, presence: true
+  belongs_to :user
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
   validates :description, presence: true, length: { minimum: 10, maximum: 300 }
-  def new
-  end
+  validates :user_id, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_20_002250) do
+ActiveRecord::Schema.define(version: 2020_05_20_005406) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "posts", force: :cascade do |t|
